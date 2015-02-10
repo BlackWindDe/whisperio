0c97ab83cecac86a777a27685cb9f3475815e5da
Maxime <maxime.escourbiac@gmail.com>
2015-02-10 14:50:50 +0100

    fix date fields.

--- Database/initTestEnvironment.sql
+++ Database/initTestEnvironment.sql
@@ -25,28 +25,28 @@ VALUES (6, "remi.serein@agilebooster.com", "Remi", "SEREIN");
 
 /* --- Projects --- */
 INSERT INTO Projects(ID, Name, KeyName, Description, CreationDate)
-VALUES (1, "AgileBooster", "ABR", "Agile Planning tool developped with Java EE Technologie.", "01/05/2014");
+VALUES (1, "AgileBooster", "ABR", "Agile Planning tool developped with Java EE Technologie.", "2014-05-20");
 
 /* --- Releases --- */
 INSERT INTO Releases(ID, Name, StartDate, EndDate, ProjectID, IsActive)
-VALUES (1, "MVP1", "01/05/2014", "01/08/2014", 1, true);
+VALUES (1, "MVP1", "2014-05-01", "2014-08-01", 1, true);
 INSERT INTO Releases(ID, Name, StartDate, EndDate, ProjectID, IsActive)
-VALUES (2, "MVP2", "02/08/2014", "01/12/2014", 1, false);
+VALUES (2, "MVP2", "2014-08-02", "2014-12-01", 1, false);
 
 /* --- Sprint --- */
 INSERT INTO Sprints(ID, Name, StartDate, EndDate, ReleaseID, IsActive)
-VALUES (1, "Sprint1", "01/05/2014", "01/06/2014", 1, false);
+VALUES (1, "Sprint1", "2014-05-01", "2014-06-01", 1, false);
 INSERT INTO Sprints(ID, Name, StartDate, EndDate, ReleaseID, IsActive)
-VALUES (2, "Sprint2", "02/06/2014", "01/07/2014", 1, true);
+VALUES (2, "Sprint2", "2014-06-02", "2014-07-01", 1, true);
 INSERT INTO Sprints(ID, Name, StartDate, EndDate, ReleaseID, IsActive)
-VALUES (3, "Sprint3", "02/07/2014", "01/08/2014", 1, false);
+VALUES (3, "Sprint3", "2014-07-02", "2014-08-01", 1, false);
 
 /* --- Backlog Items --- */
 INSERT INTO BacklogItems(ID, Title, Description, CreationDate, LastUpdateDate, CreatorID, ProjectID, ReleaseID, SprintID)
-VALUES(1, "Backlog Item 1", "Backlog Item1 Description", "02/07/2014", "04/07/2014", 2, 1, null, null);
-INSERT INTO BacklogItems(ID, Title, StoryType, CreationDate, LastUpdateDate, CreatorID, ProjectID, ReleaseID, SprintID)
-VALUES(2, "Backlog Item 2", "Backlog Item2 Description", "06/07/2014", "09/07/2014", 1, 1, 1, null);
-INSERT INTO BacklogItems(ID, Title, StoryType, Status, Description, UserField1, UserField2, CreationDate, LastUpdateDate, CreatorID, ProjectID, ReleaseID, SprintID)
-VALUES(3, "Backlog Item 3", "Backlog Item3 Description", "03/07/2014", "10/07/2014", 3, 1, 1, 1);
-INSERT INTO BacklogItems(ID, Title, StoryType, Status, Description, UserField1, UserField2, CreationDate, LastUpdateDate, CreatorID, ProjectID, ReleaseID, SprintID)
-VALUES(4, "Backlog Item 4", "Backlog Item4 Description", "08/07/2014", "12/07/2014", 2, 1, 1, 2);
+VALUES(1, "Backlog Item 1", "Backlog Item1 Description", "2014-07-02 15:00:00", "2014-07-04 13:00:00", 2, 1, null, null);
+INSERT INTO BacklogItems(ID, Title, Description, CreationDate, LastUpdateDate, CreatorID, ProjectID, ReleaseID, SprintID)
+VALUES(2, "Backlog Item 2", "Backlog Item2 Description", "2014-07-06 16:00:00", "2014-07-09 14:00:00", 1, 1, 1, null);
+INSERT INTO BacklogItems(ID, Title, Description, CreationDate, LastUpdateDate, CreatorID, ProjectID, ReleaseID, SprintID)
+VALUES(3, "Backlog Item 3", "Backlog Item3 Description", "2014-07-03 17:00:00", "2014-07-10 15:00:00", 3, 1, 1, 1);
+INSERT INTO BacklogItems(ID, Title, Description, CreationDate, LastUpdateDate, CreatorID, ProjectID, ReleaseID, SprintID)
+VALUES(4, "Backlog Item 4", "Backlog Item4 Description", "2014-07-08 18:00:00", "2014-07-12 16:00:00", 2, 1, 1, 2);
