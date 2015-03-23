08cb05c1c9a829b41b97109c584a8250abe53ffe
Maxime <mescourbiac@humanbooster.com>
2015-03-23 12:03:44 +0100

    Product backlog boxes

--- src/main/java/com/whisperio/view/ProjectBean.java
+++ src/main/java/com/whisperio/view/ProjectBean.java
@@ -11,6 +11,7 @@
 package com.whisperio.view;
 
 import com.whisperio.data.entity.BacklogItem;
+import com.whisperio.data.entity.ProductBacklogBox;
 import com.whisperio.data.entity.Project;
 import com.whisperio.data.entity.Release;
 import com.whisperio.data.entity.Sprint;
@@ -19,9 +20,10 @@ import com.whisperio.data.jpa.ReleaseController;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
@@ -35,16 +37,27 @@ import org.primefaces.model.chart.LineChartModel;
  * @author Maxime ESCOURBIAC
  */
 @ManagedBean(name = "projectBean")
-@RequestScoped
+@SessionScoped
 public class ProjectBean implements Serializable {
 
     @ManagedProperty(value = "#{sessionBean}")
     private SessionBean sessionBean;
 
+    private ProductBacklogBox selectedBox;
+    private List<BacklogItem> selectedBoxBacklogItems;
+
     /**
      * Creates a new instance of projectsBean
      */
     public ProjectBean() {
+    }
+
+    /**
+     * Post Contruct method.
+     */
+    @PostConstruct
+    public void init() {
+        selectedBox = ProductBacklogBox.CULTUREBOX;
     }
 
     /**
@@ -241,11 +254,6 @@ public class ProjectBean implements Serializable {
         return businessValueChart;
     }
 
-    public List<BacklogItem> getSelectedBoxItems() {
-        ProjectController projectController = new ProjectController();
-        return projectController.getCultureboxItems(sessionBean.getSelectedProject());
-    }
-
     /**
      * Session Bean.
      *
@@ -264,4 +272,84 @@ public class ProjectBean implements Serializable {
         this.sessionBean = sessionBean;
     }
 
+    /**
+     * Use Sandbox as selected product backlog box.
+     */
+    public void useSandbox() {
+        this.selectedBox = ProductBacklogBox.SANDBOX;
+    }
+
+    /**
+     * Use Icebox as selected product backlog box.
+     */
+    public void useIcebox() {
+        this.selectedBox = ProductBacklogBox.ICEBOX;
+    }
+
+    /**
+     * Use Culturebox as selected product backlog box.
+     */
+    public void useCulturebox() {
+        this.selectedBox = ProductBacklogBox.CULTUREBOX;
+    }
+
+    /**
+     * Use Startbox as selected product backlog box.
+     */
+    public void useStartbox() {
+        this.selectedBox = ProductBacklogBox.STARTBOX;
+    }
+
+    /**
+     * Use Sprintbox as selected product backlog box.
+     */
+    public void useSprintbox() {
+        this.selectedBox = ProductBacklogBox.SPRINTBOX;
+    }
+
+    /**
+     * Use Harvestbox as selected product backlog box.
+     */
+    public void useHarvestbox() {
+        this.selectedBox = ProductBacklogBox.HARVESTBOX;
+    }
+
+    /**
+     * Get Selected backlog items.
+     *
+     * @return Selected backlog items.
+     */
+    public List<BacklogItem> getSelectedBoxBacklogItems() {
+        ProjectController projectController = new ProjectController();
+        switch (selectedBox) {
+            case SANDBOX:
+                selectedBoxBacklogItems = projectController.getSandboxItems(sessionBean.getSelectedProject());
+                break;
+            case ICEBOX:
+                selectedBoxBacklogItems = projectController.getIceboxItems(sessionBean.getSelectedProject());
+                break;
+            case CULTUREBOX:
+                selectedBoxBacklogItems = projectController.getCultureboxItems(sessionBean.getSelectedProject());
+                break;
+            case STARTBOX:
+                selectedBoxBacklogItems = projectController.getStartboxItems(sessionBean.getSelectedProject());
+                break;
+            case SPRINTBOX:
+                selectedBoxBacklogItems = projectController.getSprintboxItems(sessionBean.getSelectedProject());
+                break;
+            case HARVESTBOX:
+                selectedBoxBacklogItems = projectController.getHarvestboxItems(sessionBean.getSelectedProject());
+                break;
+        }
+        return selectedBoxBacklogItems;
+    }
+
+    /**
+     * Set Selected backlog items.
+     *
+     * @param selectedBoxBacklogItems Selected backlog items.
+     */
+    public void setSelectedBoxBacklogItems(List<BacklogItem> selectedBoxBacklogItems) {
+        this.selectedBoxBacklogItems = selectedBoxBacklogItems;
+    }
 }
